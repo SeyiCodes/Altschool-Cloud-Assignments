6a23d07b8249d2c070850e2f42f1ed04c29e9480
Oluwaseyi Akinnawo <Oluwaseyi_akinnawo@rocketmail.com>
2023-01-11 10:21:29 +0100

    Update setup.sh

--- third-semester/Holiday_Challenge/files/setup.sh
+++ third-semester/Holiday_Challenge/files/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
-sudo apt update 
-sudo apt upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" 
-sudo apt autoremove -y
-sudo apt install -y nginx
+sudo apt update -y
+sudo NEEDRESTART_MODE=a apt dist-upgrade
+sudo apt autoremove 
+sudo NEEDRESTART_MODE=a sudo apt-get install -y nginx 
 sudo su
 
 echo "<h1>My server Hostname or IP address is $(hostname -f)</h1>" > /var/www/html/index.nginx-debian.html
