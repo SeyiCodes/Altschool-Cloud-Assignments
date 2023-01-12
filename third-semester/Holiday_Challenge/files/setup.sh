a1f878e6e23721b73c76f8dc17b938717af0caf1
Oluwaseyi Akinnawo <Oluwaseyi_akinnawo@rocketmail.com>
2023-01-12 15:11:02 +0100

    Update setup.sh

--- third-semester/Holiday_Challenge/files/setup.sh
+++ third-semester/Holiday_Challenge/files/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-export DEBIAN_FRONTEND=noninteractive
-sudo apt update -y
-sudo NEEDRESTART_MODE=a apt dist-upgrade
-sudo apt autoremove 
-sudo NEEDRESTART_MODE=a sudo apt-get install -y nginx 
-sudo su
+sudo apt update
+
+sudo apt install nginx -y
+
+sudo systemctl start nginx
+
+Sudo apt install php8.0-fpm -y
 
 echo "<h1>My server Hostname or IP address is $(hostname -f)</h1>" > /var/www/html/index.nginx-debian.html
 
