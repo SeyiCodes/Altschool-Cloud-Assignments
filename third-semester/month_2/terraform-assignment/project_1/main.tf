8ada870dba17cc78bdeddf26bec1ba3c3d2d182a
Your Name <you@example.com>
2023-02-07 14:14:32 +0100

    readme

--- third-semester/month_2/terraform-assignment/project_1/main.tf
+++ third-semester/month_2/terraform-assignment/project_1/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 
 resource "local_file" "public_ip" {
-  filename = "~/altschool/semester-projects/Altschool-Cloud-Assignments/third-semester/month_2/terraform-assignment/project_1/ansible/host-inventory"
+  filename ="./ansible/host-inventory"
   content  = <<EOT
   ${aws_instance.my_server-1.public_ip}
   ${aws_instance.my_server-2.public_ip}
